6250b3e34ff2dc68903aeb98d4c4cc11265ea16d
Santiago Lorenzatti <slorenzatti@facultad.sanfrancisco.utn.edu.ar>
2025-06-12 20:38:48 -0300

    Creacion del registro de Usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useState } from 'react';
 import Login from './components/Login';
+import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 import VistaAnual from './components/Calendar/VistaAnual';
 import VistaMensual from './components/Calendar/VistaMensual';
 import VistaSemanal from './components/Calendar/VistaSemanal';
-// import VistaDiaria from './components/Calendar/VistaDiaria';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -13,12 +13,13 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={ isAuthenticated ? (<Navigate to="/dashboard" />) : (<Login onLoginSuccess={() => setIsAuthenticated(true)} />)}/>
+
+      <Route path='register' element={<Register />}/>
       
       <Route path="/dashboard" element={ isAuthenticated ? <Dashboard /> : <Navigate to="/" />}>
         <Route index element={<VistaAnual />} />
         <Route path="mensual" element={<VistaMensual />} />
         <Route path="semanal" element={<VistaSemanal />} />
-        
         {/* <Route path="diaria" element={<VistaDiaria />} /> */}
       </Route>
       
